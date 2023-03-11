8dc61e027250df5fc5bf402c79fee175f786185f
Somtuzy <somtuzyonyeka@gmail.com>
2023-03-11 21:49:59 +0800

    Feat: This logs a user in

--- services/bcrypt.service.js
+++ services/bcrypt.service.js
@@ -5,6 +5,8 @@ const rounds = parseFloat(process.env.ROUNDS)
 const hashPassword = async (passwordInput) => {
     return await bcrypt.hash(passwordInput, rounds)
 }
+const verifyPassword = async (passwordInput, hashedPassword) => {
+    return await bcrypt.compare(passwordInput, hashedPassword)
+}
 
-
-module.exports = hashPassword;
+module.exports = { hashPassword, verifyPassword }
