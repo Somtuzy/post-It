bfe4ed49bdb951c69b9f66a9dc9574afc620ca2e
Somtuzy <somtuzyonyeka@gmail.com>
2023-03-11 23:19:27 +0800

    Feat: This adds a feature to fetch a postit

--- routes/postit.route.js
+++ routes/postit.route.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const authenticate = require('../middlewares/authentication')
-const { createPost, updatePost, deletePost } = require('../controllers/postit.controller')
+const { createPost, updatePost, deletePost, getPost } = require('../controllers/postit.controller')
 const { validatePostitInputs } = require('../middlewares/validate')
 
 const router = Router()
@@ -10,6 +10,7 @@ router.route('/postits')
 
 router.route('/postits/:id')
 .put(authenticate, updatePost)
+.get(authenticate, getPost)
 .delete(authenticate, deletePost)
 
 module.exports = router;
