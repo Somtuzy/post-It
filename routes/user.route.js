a69a949018d2c6273a7dbe50d03aae0f642f1f52
Somtuzy <somtuzyonyeka@gmail.com>
2023-03-11 22:18:03 +0800

    Feat: This adds a feature to fetch a user by their id

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { updateUser, deleteUser } = require('../controllers/user.controller') 
+const { updateUser, deleteUser, getUser } = require('../controllers/user.controller') 
 const authenticate = require('../middlewares/authentication')
 
 const router = Router()
@@ -7,5 +7,6 @@ const router = Router()
 router.route('/users/:id')
 .put(authenticate, updateUser)
 .delete(authenticate, deleteUser)
+.get(authenticate, getUser)
 
 module.exports = router;
