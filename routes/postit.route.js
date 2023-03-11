a8929aa23d3785cdb4986e5100adb0eccd2873cd
Somtuzy <somtuzyonyeka@gmail.com>
2023-03-11 23:28:01 +0800

    Feat: This adds a feature to fetch all postits

--- routes/postit.route.js
+++ routes/postit.route.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const authenticate = require('../middlewares/authentication')
-const { createPost, updatePost, deletePost, getPost } = require('../controllers/postit.controller')
+const { createPost, updatePost, deletePost, getPost, getPosts } = require('../controllers/postit.controller')
 const { validatePostitInputs } = require('../middlewares/validate')
 
 const router = Router()
 
 router.route('/postits')
 .post(authenticate, validatePostitInputs, createPost)
+.get(authenticate, getPosts)
 
 router.route('/postits/:id')
 .put(authenticate, updatePost)
