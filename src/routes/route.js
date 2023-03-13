68e0be733c0623e2d644d2450ad7a310a069c782
Somtuzy <somtuzyonyeka@gmail.com>
2023-03-13 11:16:25 +0800

    Feat: This adds a redirect feature for documentation

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const signinRouter = require('./user.signin.route')
 const userRouter = require('./user.route')
 const postitRouter = require('./postit.route')
 const commentRouter = require('./comment.route')
+const docsUrl = process.env.DOCS_URL
 
 const router = Router()
 
@@ -10,5 +11,10 @@ router.use('/v1', signinRouter)
 router.use('/v1', userRouter)
 router.use('/v1', postitRouter)
 router.use('/v1', commentRouter)
+
+// Redirects to API documentation
+router.use('/v1/docs', (req, res) => {
+    res.redirect(docsUrl)
+})
 
 module.exports = router;
